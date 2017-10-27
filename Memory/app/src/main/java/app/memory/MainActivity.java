d6ebbe67af60833ad0ab1ef44f3ff409de14ba05
Austin Paul <safeat3rd@hotmail.com>
2017-10-26 22:01:45 -0500

    some more changes

--- Memory/app/src/main/java/app/memory/MainActivity.java
+++ Memory/app/src/main/java/app/memory/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button start_button = (Button) findViewById(R.id.start_button);
+        Button leaderboard_button = (Button) findViewById(R.id.leaderboard_button);
+
 
     }
 }
