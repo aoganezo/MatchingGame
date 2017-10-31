25202c0f34cc4602e43254739fa419d357e34a26
artur oganezov <artur@loop-depaulsecure-235-23.depaulsecure-student.depaul.edu>
2017-10-31 18:02:07 -0500

    Some work was completed.

--- Memory/app/src/main/java/app/memory/GamePlay.java
+++ Memory/app/src/main/java/app/memory/GamePlay.java
@@ -6,13 +6,42 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+import android.widget.ImageView;
 
 public class GamePlay extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
+
+        int size;
+
+        Intent intent = getIntent();
+        final String mode = intent.getStringExtra("mode");
+
+
+        if (mode == "2ez") {
+            size = 10;
+        }
+        else if (mode == "ez") {
+            size = 16;
+        }
+        else if (mode == "not ez") {
+            size = 24;
+        }
+        else {
+            size = 0;
+        }
+
+//        for (int i = 0; i < size; i++) {
+//            ImageView aView = new ImageView();
+//
+//        }
+
+
+
+
 
 
     }
