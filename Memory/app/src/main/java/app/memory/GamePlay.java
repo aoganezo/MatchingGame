70b5935a1eafe37da8a07a312e8b6b37cf90c1d8
artur oganezov <artur@loop-depaulsecure-235-23.depaulsecure-student.depaul.edu>
2017-10-31 18:32:52 -0500

    Game work, project cleanup.

--- Memory/app/src/main/java/app/memory/GamePlay.java
+++ Memory/app/src/main/java/app/memory/GamePlay.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.content.Intent;
-import android.widget.ImageView;
+
+import android.widget.GridView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class GamePlay extends AppCompatActivity {
     @Override
@@ -34,10 +37,20 @@ public class GamePlay extends AppCompatActivity {
             size = 0;
         }
 
-//        for (int i = 0; i < size; i++) {
-//            ImageView aView = new ImageView();
-//
-//        }
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View v, int position, long id){
+                // Send intent to SingleViewActivity
+                Intent i = new Intent(getApplicationContext(), SingleViewActivity.class);
+                // Pass image index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+
 
 
 
