204c8f161efa0b44605f12bee7e5c1896f79b780
Austin Paul <safeat3rd@hotmail.com>
2017-10-31 19:20:44 -0500

    added intents to MainActivity and DifficultyActivity

--- Memory/app/src/main/java/app/memory/DifficultyActivity.java
+++ Memory/app/src/main/java/app/memory/DifficultyActivity.java
@@ -1,7 +1,10 @@
 package app.memory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DifficultyActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class DifficultyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_difficulty);
+
+        Button two_ez_button = (Button) findViewById(R.id.two_ez_button);
+        Button ez_button = (Button) findViewById(R.id.ez_button);
+        Button not_ez_button = (Button) findViewById(R.id.not_ez_button);
+
+        View.OnClickListener listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Button button = (Button) v;
+                Intent to_gameplay = new Intent(DifficultyActivity.this, GamePlay.class);
+                to_gameplay.putExtra("mode", button.getText().toString());
+                startActivity(to_gameplay);
+            }
+        };
+
+        two_ez_button.setOnClickListener(listener);
+        ez_button.setOnClickListener(listener);
+        not_ez_button.setOnClickListener(listener);
+
     }
+
 }
--- Memory/app/src/main/java/app/memory/MainActivity.java
+++ Memory/app/src/main/java/app/memory/MainActivity.java
@@ -1,7 +1,9 @@
 package app.memory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,25 @@ public class MainActivity extends AppCompatActivity {
 
         Button start_button = (Button) findViewById(R.id.start_button);
         Button leaderboard_button = (Button) findViewById(R.id.leaderboard_button);
+
+        View.OnClickListener start_listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent to_difficulty = new Intent(MainActivity.this, DifficultyActivity.class);
+                startActivity(to_difficulty);
+            }
+        };
+
+        View.OnClickListener leaderboard_listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent to_leaderboard = new Intent(MainActivity.this, LeaderboardActivity.class);
+                startActivity(to_leaderboard);
+            }
+        };
+
+        start_button.setOnClickListener(start_listener);
+        leaderboard_button.setOnClickListener(leaderboard_listener);
 
 
     }
