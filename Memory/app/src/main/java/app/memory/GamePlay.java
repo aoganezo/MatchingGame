13688986ff059695bf8b08bc0c3707799abed052
artur oganezov <artur@dhcp-10-103-251-244.wireless.northwestern.private>
2017-11-03 20:19:57 -0500

    game layout progress

--- Memory/app/src/main/java/app/memory/GamePlay.java
+++ Memory/app/src/main/java/app/memory/GamePlay.java
@@ -12,6 +12,12 @@ import android.content.Intent;
 import android.widget.GridView;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class GamePlay extends AppCompatActivity {
     GridView gridView;
@@ -38,7 +44,9 @@ public class GamePlay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
 
-        /*int size;
+
+        /*
+        int size;
 
         Intent intent = getIntent();
         final String mode = intent.getStringExtra("mode");
@@ -59,12 +67,76 @@ public class GamePlay extends AppCompatActivity {
         else {
             Log.i("test", "test1, gameplay, size=error");
             size = 0;
-        }*/
+        }
+        */
 
-        gridView = (GridView) findViewById(R.id.gridview);
+        ImageView iv1 = (ImageView) findViewById(R.id.iv1);
+        ImageView iv2 = (ImageView) findViewById(R.id.iv2);
+        ImageView iv3 = (ImageView) findViewById(R.id.iv3);
+        ImageView iv4 = (ImageView) findViewById(R.id.iv4);
+        ImageView iv5 = (ImageView) findViewById(R.id.iv5);
+        ImageView iv6 = (ImageView) findViewById(R.id.iv6);
+        ImageView iv7 = (ImageView) findViewById(R.id.iv7);
+        ImageView iv8 = (ImageView) findViewById(R.id.iv8);
+        ImageView iv9 = (ImageView) findViewById(R.id.iv9);
+        ImageView iv10 = (ImageView) findViewById(R.id.iv10);
+        ImageView iv11 = (ImageView) findViewById(R.id.iv11);
+        ImageView iv12 = (ImageView) findViewById(R.id.iv12);
+
+        Integer imageIds = {00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12};
+
+        int firstCard, secondCard;
+        int clickedFirst, clickedSecond;
+        int cardNumber = 1;
+
+        int turn = 1;
+
+        TextView score = (TextView) findViewById(R.id.score);
+
+        iv1.setTag("0");
+        iv1.setTag("1");
+        iv1.setTag("2");
+        iv1.setTag("3");
+        iv1.setTag("4");
+        iv1.setTag("5");
+        iv1.setTag("6");
+        iv1.setTag("7");
+        iv1.setTag("8");
+        iv1.setTag("9");
+        iv1.setTag("10");
+        iv1.setTag("11");
+
+
+        frontOfCardResources();
+
+        Collections.shuffle(Arrays.asList(imageIds));
+
+        
+    }
+    private void frontOfCardResources() {
+        int img01, img02, img03, img04, img05, img06, img07, img08, img09, img10, img11, img12;
+        img01 = R.drawable.hippo;
+        img02 = R.drawable.hippo;
+        img03 = R.drawable.hippo;
+        img04 = R.drawable.hippo;
+        img05 = R.drawable.hippo;
+        img06 = R.drawable.hippo;
+        img07 = R.drawable.hippo;
+        img08 = R.drawable.hippo;
+        img09 = R.drawable.hippo;
+        img10 = R.drawable.hippo;
+        img11 = R.drawable.hippo;
+        img12 = R.drawable.hippo;
+
+    }
+
+
+
+
+
+
+
         Log.i("test", "test1, gameplay, gridview created");
-        ImageAdapter gridAdapter = new ImageAdapter(this, values, images);
-        gridView.setAdapter(gridAdapter);
 //
 //        gridView.setOnItemClickListener(new OnItemClickListener() {
 //            public void onItemClick(AdapterView<?> parent,
